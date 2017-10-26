9b2adb12ad33149ac5bc2987145983cc39933250
Anderson Martins <andersonom@gmail.com>
2017-10-26 10:32:55 -0200

    More algorithms

---
+++ dotnet/FW 4.6/Algorithms/Algorithms/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Client
+    {
+        public enum TipoCliente { Gold = 1, Silver = 2 }
+
+        public class Cliente
+        {
+            public int ClienteId { get; set; }
+            public string Nome { get; set; }
+            public DateTime DataCadastro { get; set; }
+            public TipoCliente TipoCliente { get; set; }
+            public virtual bool ClienteEspecial()
+            {
+                return DataCadastro.Year < 2010;
+            }
+            public virtual decimal CalcularDesconto(decimal valor)
+            {
+                switch (TipoCliente)
+                {
+                    case TipoCliente.Gold:
+                        return valor * 0.40m; //desconto de 60%
+                    case TipoCliente.Silver:
+                        return valor * 0.80m; //Desconto de 20%
+                    default:
+                        return valor;
+                }
+            }
+        }
+
+        public class PossivelCliente : Cliente
+        {
+            public int PossivelClienteId { get; set; }
+
+            public override decimal CalcularDesconto(decimal valor)
+            {
+                return base.CalcularDesconto(valor);
+            }
+
+            public override bool ClienteEspecial()
+            {
+                //Ops, eu ainda não sou cliente, muito menos especial
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ dotnet/FW 4.6/Algorithms/Algorithms/LittleShino.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class LittleShino
+    {//GetNumbersDivideBytwoNumbers
+        public static void Shino()
+        {
+            /*Little Shino loves maths. Today her teacher gave her two integers. 
+        Shino is now wondering how many integers can divide both the numbers. 
+              She is busy with her assignments. Help her to solve the problem.*/
+
+            string input = Console.ReadLine();// Read two integers
+
+            #region Assign n1 and n2 variables
+            char[] inputChars = input.ToCharArray();
+
+            int spacePos = 0;
+
+            for (int i = 0; i < inputChars.Length; i++)
+            {
+                if (inputChars[i].Equals(' '))
+                    spacePos = i;
+            }
+
+            int n1 = int.Parse(input.Substring(0, spacePos));
+            var n2 = int.Parse(input.Substring(spacePos + 1, input.Length - (spacePos + 1)));
+            #endregion
+
+            List<int> divNum1 = new List<int>();
+            List<int> divNum2 = new List<int>();
+
+            for (int i = 1; i <= n1; i++)
+            {
+                if (n1 % i == 0)//If can divide 
+                {
+                    divNum1.Add(i);//Add numbers that first number can be divide
+                }
+            }
+
+            for (int i = 1; i <= n2; i++)
+            {
+                if (n2 % i == 0)// If can divide
+                {
+                    divNum2.Add(i);//Add numbers that second number can be divide
+                }
+            }
+            List<int> result = new List<int>();
+
+
+
+            foreach (var i1 in divNum1)
+            {
+                foreach (var i2 in divNum2)
+                {
+                    if (i2 == i1)
+                        result.Add(i1);//Add number if it contains in both list
+                }
+            }
+
+        }
+    }
+}

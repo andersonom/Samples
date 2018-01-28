60ff73d511c3423779eb273b59a204839c44f6f5
Anderson Martins <andersonom@gmail.com>
2018-01-28 00:01:23 -0200

    Agile Content test

---
+++ dotnet/core/2.0/AgileContent/AgileContent.Tests/AgileContentTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgileContent.API.Controllers;
+using AgileContent.Core;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgileContent.Tests
+{
+    [TestClass()]
+    public class AgileContentTests
+    {
+        Sibling _sibling;
+        CdnConverterController _apiCDNController;
+
+        public AgileContentTests()
+        {
+            _sibling = new Sibling();
+            _apiCDNController = new CdnConverterController(null, new CDN());
+        }
+
+        [TestMethod()]
+        public void SiblingsTest()//Ex 1
+        {
+            Assert.AreEqual(_sibling.GetBiggestSibling(355), 553);
+            Assert.AreEqual(_sibling.GetBiggestSibling(213), 321);
+            Assert.AreEqual(_sibling.GetBiggestSibling(553), 553);
+            Assert.AreEqual(_sibling.GetBiggestSibling(-10), 0);
+            Assert.AreEqual(_sibling.GetBiggestSibling(100000001), -1);
+            Assert.AreEqual(_sibling.GetBiggestSibling(100000000), 100000000);
+        }
+
+        [TestMethod()]
+        public void CDNAPITest()//Ex 2
+        {
+            var result = _apiCDNController.Post("https://s3.amazonaws.com/uux-itaas-static/minha-cdn-logs/input-01.txt", $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\AgileContentCDN.txt") 
+                as OkResult;            
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ dotnet/core/2.0/AgileContent/AgileContent/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AgileContent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ex 1
+            new Core.Sibling().GetBiggestSibling(355);
+
+            //Ex 2
+            if (args.Length == 2)
+            {
+                if (new Core.CDN().Converter("MINHA CDN", args[0], args[1]))
+                    Console.WriteLine($"{args[1]} wrote sucessfully");
+            }
+            else
+            {
+                var target = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\AgileContentCDN.txt";
+
+                if (new Core.CDN().Converter("MINHA CDN", "https://s3.amazonaws.com/uux-itaas-static/minha-cdn-logs/input-01.txt", target))
+                    Console.WriteLine($"{target} wrote sucessfully");
+            }
+        }
+    }
+}
